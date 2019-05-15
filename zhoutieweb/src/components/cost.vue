49b139e9b239e764075f61c8df41e00cb54f95e5
dianrenjie <1045899041@qq.com>
2019-05-15 16:34:09 +0800

    sdfsd

--- zhoutieweb/src/components/cost.vue
+++ zhoutieweb/src/components/cost.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="app">
-    <el-table :data="tableData" style="width: 100%">
+    <el-table :data="tableData" style="width: 100%;margin:auto;">
       <el-table-column prop="date" label="序号" width="100"></el-table-column>
 
       <el-table-column prop="name" label="姓名" width="100"></el-table-column>
