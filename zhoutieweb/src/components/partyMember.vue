a68ff4d706bb55983837a38d96b7d344673b31af
shixiongshan <1054880941@qq.com>
2019-05-20 15:17:36 +0800

    党员名册数据填充，以及分页插件已写好

--- zhoutieweb/src/components/partyMember.vue
+++ zhoutieweb/src/components/partyMember.vue
@@ -1,17 +1,10 @@
 <template>
   <div>
-    <el-table ref="filterTable" :data="tableData" style="width: 80%;margin: 20px auto;">
+    <el-table ref="filterTable" :data="list" style="width: 80%;margin: 20px auto;">
+      <el-table-column prop="id" label="序号" width="180" column-key="id"></el-table-column>
+      <el-table-column prop="realname" label="党员名称" width="180"></el-table-column>
       <el-table-column
-        prop="number"
-        label="序号"
-        width="180"
-        column-key="number"
-        :filters="[{text: '1', value: '1'}, {text: '2', value: '2'}, {text: '3', value: '3'}, {text: '4', value: '4'}]"
-        :filter-method="filterHandler"
-      ></el-table-column>
-      <el-table-column prop="name" label="党员名称" width="180"></el-table-column>
-      <el-table-column
-        prop="sex"
+        prop="gender"
         label="性别"
         width="100"
         :filters="[{ text: '男', value: '男' }, { text: '女', value: '女' }]"
@@ -20,15 +13,19 @@
       >
         <template slot-scope="scope">
           <el-tag
-            :type="scope.row.sex === '男' ? 'primary' : 'success'"
+            :type="scope.row.gender === '男' ? 'primary' : 'success'"
             disable-transitions
-          >{{scope.row.sex}}</el-tag>
+          >{{scope.row.gender}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column prop="birthday" label="出生日期" :formatter="formatter"></el-table-column>
+      <el-table-column prop="birth" label="出生日期" :formatter="formatter"></el-table-column>
       <el-table-column prop="education" label="学历" :formatter="education"></el-table-column>
-      <el-table-column prop="partyTime" label="入党时间" :formatter="partyTime"></el-table-column>
+      <el-table-column prop="passtime" label="入党时间" :formatter="passtime"></el-table-column>
     </el-table>
+    <!-- 分页 -->
+    <div class="block">
+      <el-pagination layout="prev, pager, next" :total="size" :current-page.sync="currentpage"></el-pagination>
+    </div>
   </div>
 </template>
 
@@ -36,44 +33,13 @@
 export default {
   data() {
     return {
-      tableData: [
-        {
-          number: "1",
-          name: "王小虎",
-          birthday: "2019-2-15",
-          sex: "男",
-          education:"硕士",
-          partyTime:"2019-5-15"
-        },
-        {
-          number: "2",
-          name: "王小虎",
-          birthday: "2019-1-15",
-          sex: "女",
-          education:"博士",
-          partyTime:"2019-2-15",
-        },
-        {
-          number: "3",
-          name: "王小虎",
-          birthday: "2019-2-15",
-          sex: "男",
-          education:"大学",
-          partyTime:"2019-3-15",
-        },
-        {
-          number: "4",
-          name: "王小虎",
-          birthday: "2019-3-15",
-          sex: "女",
-          education:"高中",
-          partyTime:"2019-4-15",
-        }
-      ]
+      list: [],
+      currentpage: 1,
+      value: "5"
     };
   },
-  mounted(){
-var _this = this;
+  mounted() {
+    var _this = this;
     var data = this.qs.stringify({ organ_id: "1" });
     this.$axios({
       url: "/api/api/member_list",
@@ -86,22 +52,37 @@ var _this = this;
     });
   },
   methods: {
-    
-    partyTime(row, column) {
-      return row.partyTime;
-    }, 
+    passtime(row, column) {
+      return row.passtime;
+    },
     education(row, column) {
       return row.education;
-    }, 
+    },
     formatter(row, column) {
-      return row.birthday;
+      return row.birth;
     },
     filterTag(value, row) {
-      return row.sex === value;
+      return row.gender === value;
+    }
+  },
+  computed: {
+    //分页
+    arr() {
+      var currentpage = this.currentpage;
+      var arr2 = [];
+      var size = this.value;
+      for (var i = currentpage * size - size; i < size * currentpage; i++) {
+        if (i < this.list.length) {
+          arr2.push(this.list[i]);
+        }
+      }
+      return arr2;
     },
-    filterHandler(value, row, column) {
-      const property = column["property"];
-      return row[property] === value;
+    size() {
+      var len = this.list.length;
+      var size;
+      size = Math.ceil(len / this.value) * 10;
+      return size;
     }
   }
 };
